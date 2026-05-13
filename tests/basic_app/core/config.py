07d6c6663634b955a96effa8654b0a9ec586d0eb
Sahil Nayak <contactsahilpnayak@gmail.com>
2026-05-14 00:28:36 +0530

    test: basic scaffolding app

---
+++ tests/basic_app/core/config.py
@@ -0,0 +1,15 @@
+from pydantic_settings import BaseSettings
+from functools import lru_cache
+
+
+class Settings(BaseSettings):
+    APP_NAME: str = "basic_app"
+    DEBUG: bool = False
+    
+    class Config:
+        env_file = ".env"
+
+
+@lru_cache()
+def get_settings():
+    return Settings()

f5f95a1c0df369aebee7a8d40f5fb5b9bb7ab1b2
sahilnyk <contactsahilpnayak@gmail.com>
2026-09-01 22:34:50 +0530

    style: sort package imports

--- fullapi/__init__.py
+++ fullapi/__init__.py
@@ -1,6 +1,6 @@
 """fullapi - FastAPI project scaffolder."""
 
-from importlib.metadata import version, PackageNotFoundError
+from importlib.metadata import PackageNotFoundError, version
 
 try:
     __version__ = version("fullapi")
